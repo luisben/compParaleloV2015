4ab04285c2fce6d5fce95d68f1a55e2e64aad87d
luis.benruiz <luis.benruiz@gmail.com>
2015-06-25 00:28:19 -0500

    fixed P7 as it was doing two-sum vector instead os umming all elements in one vector completed P8

--- MPI/MPI_P7_SumaElementosVector.c
+++ MPI/MPI_P7_SumaElementosVector.c
@@ -10,50 +10,42 @@ int size = 1000;
 int splitsize;
 //make arrays
 int *a = malloc(size * sizeof(int));
-int *b = malloc(size * sizeof(int));
-int *result = malloc(size * sizeof(int));
+int result = 0;
 int idx = 0;
+int sub_result = 0;
 
 for(idx = 0;idx <size;idx++){
     a[idx]=idx;
-    b[idx]=idx*2;
 }
 
 MPI_Init(&argc,&argv);
 MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
 MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-MPI_Get_processor_name(processor_name,&namelen);
+
 splitsize = (int) size/numprocs;
 int *sub_a = malloc(splitsize * sizeof(int));
+int *sub_results = malloc(numprocs * sizeof(int));
+
 MPI_Scatter(a,splitsize,MPI_INT,sub_a,splitsize,MPI_INT,0,MPI_COMM_WORLD);
 
-int *sub_b = malloc(splitsize * sizeof(int));
-MPI_Scatter(b,splitsize,MPI_INT,sub_b,splitsize,MPI_INT,0,MPI_COMM_WORLD);
-
-int *sub_result = malloc(splitsize * sizeof(int));
-
-idx = 0;
-
 for(idx=0;idx<splitsize;idx++)
-    sub_result[idx] = sub_a[idx]+sub_b[idx];
+    sub_result += sub_a[idx];
 
+MPI_Gather(&sub_result,1,MPI_INT,sub_results,1,MPI_INT,0,MPI_COMM_WORLD);
 
-MPI_Gather(sub_result,splitsize,MPI_INT,result,splitsize,MPI_INT,0,MPI_COMM_WORLD);
+for(idx=0;idx<numprocs;idx++)
+    result += sub_results[idx];
 
 MPI_Finalize();
 
 if(rank==0){
-        for(idx=0;idx<size;idx+=(int) size/10)
-                printf("\n results at %i  %i+%i=%i \n ",idx,a[idx],b[idx],result[idx]);
+	printf("\n results is %i, should be %i  \n ",result,(size*(size-1))/2);
 }
 
 free(a);
-free(b);
-free(result);
 
 free(sub_a);
-free(sub_b);
-free(sub_result);
+free(sub_results);
 
 return 0;
 }
