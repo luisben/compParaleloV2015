6f169c39327d99057a5da5f4518d7fe6b4e37695
luisben <luis.benruiz@gmail.com>
2015-06-26 19:27:16 -0500

    fixed parallelization for p5 and p6 for omp

--- openMP/openMP_P5_VectorXMatriz.c
+++ openMP/openMP_P5_VectorXMatriz.c
@@ -12,7 +12,7 @@ Practice 5: Parallelizing the product of 1 vector and 1 matrix.
 
 int main(){
 
-int size = 500;
+int size = 1000;
 float *mat = malloc(size * size * sizeof(float));
 float *vec = malloc(size * sizeof(float));
 float *result = malloc(size * sizeof(float));
@@ -20,7 +20,7 @@ float tmpResult;
 
 int iteration = 0;
 double total_time =0;
-for(iteration=0;iteration<10;iteration++){
+for(iteration=0;iteration<1;iteration++){
 
 int idx_rows=0,idx_cols=0;
 for(idx_rows=0;idx_rows<size;idx_rows++){
@@ -32,20 +32,23 @@ for(idx_rows=0;idx_rows<size;idx_rows++){
 printf("\n Multiply vector * matrix \n");
 double start = omp_get_wtime();
 omp_set_dynamic(0);
-omp_set_num_threads(16);
+omp_set_num_threads(1);
+
 #pragma omp parallel for \
-reduction(+:tmpResult)
+private(tmpResult)
 for(idx_rows=0;idx_rows<size;idx_rows++){
     tmpResult = 0;
+    #pragma omp parallel for \
+    reduction(+:tmpResult)
     for(idx_cols=0;idx_cols<size;idx_cols++)
         tmpResult += mat[idx_rows*size + idx_cols]*vec[idx_rows];
     result[idx_rows] = tmpResult;
 }
 double end = omp_get_wtime();
 
-/*for(idx_rows=0;idx_rows<size;idx_rows++){
+for(idx_rows=0;idx_rows<size;idx_rows+=size/10){
     printf("\n result[%i] = %f should be %i \n",idx_rows,result[idx_rows],(2*size*(idx_rows*idx_rows+idx_rows)));
-}*/
+}
 printf("\n Vector * Matrix multiplication completed successfully in %f \n",end - start);
 total_time += (end-start);
 }
--- openMP/openMP_P6_MatrizXMatriz.c
+++ openMP/openMP_P6_MatrizXMatriz.c
@@ -20,7 +20,7 @@ float tmpResult;
 
 int iteration = 0;
 double total_time =0;
-for(iteration=0;iteration<10;iteration++){
+for(iteration=0;iteration<1;iteration++){
 
 int idx_rows=0,idx_cols=0,idx_results=0;
 for(idx_rows=0;idx_rows<size;idx_rows++){
@@ -40,22 +40,24 @@ for(idx_rows=0;idx_rows<size;idx_rows++)
 */
 printf("\n Multiply matrix * matrix \n");
 double start = omp_get_wtime();
+omp_set_num_threads(4);
 for(idx_results=0;idx_results<size;idx_results++)
-#pragma omp parallel for \
-reduction(+:tmpResult) \
-num_threads(300)
+    #pragma omp parallel for \
+    private(tmpResult)
     for(idx_rows=0;idx_rows<size;idx_rows++){
         tmpResult = 0;
+        #pragma omp parallel for \
+        reduction(+:tmpResult)
         for(idx_cols=0;idx_cols<size;idx_cols++)
             tmpResult += matA[idx_results*size + idx_cols]*matB[idx_cols*size+idx_rows];
         result[idx_results*size + idx_rows] = tmpResult;
     }
 double end = omp_get_wtime();
-/*
+
 for(idx_rows=0;idx_rows<size;idx_rows++)
-    for(idx_cols=0;idx_cols<size;idx_cols++)
-        printf("\n result[%i][%i] = %f",idx_rows,idx_cols,result[idx_rows*size+idx_cols]);
-*/
+//    for(idx_cols=0;idx_cols<size;idx_cols++)
+        printf("\n result[%i][%i] = %f",idx_rows,idx_cols,result[idx_rows*size]);
+
 printf("\n Matrix * Matrix multiplication completed successfully in %f\n",end - start);
 total_time += (end-start);
 }
