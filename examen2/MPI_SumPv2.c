dd561a7b9df8eeeb00b032cb6b5a05d5cfe18a2c
luis.benruiz <luis.benruiz@gmail.com>
2015-06-30 21:10:28 -0500

    added exam files

---
+++ examen2/MPI_SumPv2.c
@@ -0,0 +1,23 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+int rank, finalP=0,myP,numProcs;
+
+MPI_Init(&argc,&argv);
+MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
+
+myP = (rank+1)+1;
+
+MPI_Reduce(&myP,&finalP,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+
+MPI_Finalize();
+
+if(rank==0)
+	printf("\nP es : %i\n",finalP);
+
+return 0;
+}
+
